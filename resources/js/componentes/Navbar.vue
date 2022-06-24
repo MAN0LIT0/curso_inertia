4291fcdae4706e44c3780d501bc51fa22dbf94bd
Marco Gondim <marcoantonioantonio949@gmail.com>
2022-06-24 18:54:30 -0300

    Active Links

--- resources/js/componentes/Navbar.vue
+++ resources/js/componentes/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
     <nav>
         <ul>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/users">Users</Link></li>
-            <li><Link href="/settings">Settings</Link></li>
+            <li><Link href="/" :class="{'font-bold': $page.component == 'Home'}">Home</Link></li>
+            <li><Link href="/users" :class="{'font-bold': $page.component == 'Users'}">Users</Link></li>
+            <li><Link href="/settings" :class="{'font-bold': $page.component == 'Settings'}">Settings</Link></li>
             <li><Link href="/logout" method="post" :data="{foo:'secso'}" as="button">Log Out</Link></li>
         </ul>
     </nav>
@@ -16,3 +16,14 @@ export default {
     }
 }
 </script>
+<style scoped>
+    a {
+        text-decoration: none;
+    }
+    .font-bold{
+        color: rgb(41, 197, 62);
+        font-weight: bold;
+        text-decoration: underline;
+    }
+
+</style>
